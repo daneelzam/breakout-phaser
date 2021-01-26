0b26c3bc4ccd50790ef6fcaad4119ded9462e856
Daniel Zameshaev <daneelzam@yandex.ru>
2021-01-26 16:59:29 +0300

    canvas setting

--- js/index.js
+++ js/index.js
@@ -2,14 +2,18 @@ const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
   preload, create, update,
 });
 
+// предзагрузка ресурсов игры
 function preload() {
-
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+  game.stage.backgroundColor = '#eee';
 }
-
+// вызывается только один раз, когда всё загружено и готово
 function create() {
 
 }
-
+// вызывается на каждом кадре
 function update() {
 
 }
